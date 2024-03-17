d0f2a62d9e292526d6611baf024166ffea00289e
Ashish <theashish752@gmail.com>
2024-03-17 17:09:53 +0530

    Style the story section and install the tailwind scrollbar

--- app/components/Stories.jsx
+++ app/components/Stories.jsx
@@ -3,9 +3,10 @@ import minifaker, { arrayElement } from "minifaker";
 import "minifaker/locales/en";
 import { useEffect, useState } from "react";
 import Story from "./Story";
+import 'tailwind-scrollbar'
 
 
-// Note: change the file name from the .tsx to .jsx because minifaker is not supporting in the typescript 
+// Note: change the file name from the .tsx to .jsx because minifaker is not supporting in the typescript
 // when I will use the actual data then I will change the file extention from the .tsx to .jsx
 
 export default function Stories() {
@@ -21,12 +22,15 @@ export default function Stories() {
   }, []);
 
   return (
-    <div>
-      <ul>
-        {storyUsers.map((user) => (
-          <Story key={user.id} id={user.id} imgSrc={user.img} username={user.username} />
-        ))}
-      </ul>
+    <div className="flex space-x-2 p-6 bg-white m-8 overflow-x-scroll scrollbar-none ">
+      {storyUsers.map((user) => (
+        <Story
+          key={user.id}
+          id={user.id}
+          imgSrc={user.img}
+          username={user.username}
+        />
+      ))}
     </div>
   );
 }
--- app/components/Story.jsx
+++ app/components/Story.jsx
@@ -1,18 +1,21 @@
-import Image from "next/image"
-import Stories from "./Stories"
+import Image from "next/image";
+import Stories from "./Stories";
 
-export default function Story({imgSrc, username}) {
-    return (
-        <div>
-            <Image
-                src={imgSrc}
-                alt="user img"
-                width={100}
-                height={100}
-            />
-            <p>
-                {username}
-                </p>
-        </div>
-  )
+export default function Story({ imgSrc, username }) {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <Image
+          src={imgSrc}
+          alt="user img"
+          width={80}
+          height={80}
+          className="rounded-full w-26 h-26 p-[1.5px] border-gray-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out "
+        />
+      </div>
+      <div>
+        <p className="text-xs text-center w-20 truncate">{username}</p>
+      </div> 
+    </div>
+  );
 }
